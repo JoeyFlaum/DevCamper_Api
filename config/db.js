46e51784a45c1ef4639f8cf6d012d50d547b8808
JoeyFlaum <58762590+Therealaveragejoe@users.noreply.github.com>
2021-12-16 22:30:47 -0600

    begin setup of starter data and schema for database

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
   const conn = await mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
   });
-  consol.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
+  console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
 };
 
 module.exports = connectDB;
