fa891b1563d8499899f9187f1848c2b1d78467e3
JoeyFlaum <58762590+Therealaveragejoe@users.noreply.github.com>
2021-12-20 23:16:49 -0600

    send data to the database. fetch data from the database

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const connectDB = require('./config/db');
-const colors = require('colors')
+const colors = require('colors');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
@@ -14,6 +14,10 @@ connectDB();
 const bootcamps = require('./routes/bootcamps');
 
 const app = express();
+
+// Body Parser
+
+app.use(express.json());
 
 //Dev Logging middleware  **********ToDo --->uncomment if statement, delete true
 if (/*process.env.NODE_ENV === 'development'*/ true) {
@@ -27,12 +31,14 @@ const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
 
 //Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
   console.log(`Error: ${err.message}`.red);
   //close server & exit process
-  server.close(()=>process.exit(1))
+  server.close(() => process.exit(1));
 });
